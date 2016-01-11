ecef7fa43d636f25903e67a5a038d143a70a20fe
Aaron Johnson <ajj@umn.edu>
2016-01-11 09:44:44 -0600

    title/meta tag change for description of site

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -191,6 +191,7 @@ export default class Main extends React.Component {
 };
 
 Main.defaultProps = {
+  // api: "http://localhost:8081/",
   api: "http://picktaco.com:8081/",
   filter_positions: {nba: [{ payload: 'PG', text: 'PG'}, { payload: 'SG', text: 'SG'}, { payload: 'SF', text: 'SF'}, { payload: 'PF', text: 'PF'}, { payload: 'C', text: 'C'}], nfl: [{ payload: 'QB', text: 'QB'}, { payload: 'RB', text: 'RB'}, { payload: 'WR', text: 'WR'}, { payload: 'TE', text: 'TE'}, { payload: 'D', text: 'D'}]},
 };
