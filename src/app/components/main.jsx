a0aced6c05ff8d97492dac113d20081cc5577a3e
Aaron Johnson <ajj@umn.edu>
2016-01-13 09:00:32 -0600

    correct api path

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -201,7 +201,7 @@ export default class Main extends React.Component {
 };
 
 Main.defaultProps = {
-  api: "http://localhost:8081/",
-  // api: "http://picktaco.com:8081/",
+  // api: "http://localhost:8081/",
+  api: "http://picktaco.com:8081/",
   filter_positions: {nba: [{ payload: 'PG', text: 'PG'}, { payload: 'SG', text: 'SG'}, { payload: 'SF', text: 'SF'}, { payload: 'PF', text: 'PF'}, { payload: 'C', text: 'C'}, { payload: 'G', text: 'G'}, { payload: 'F', text: 'F'}, { payload: 'all', text: 'All'}], nfl: [{ payload: 'QB', text: 'QB'}, { payload: 'RB', text: 'RB'}, { payload: 'WR', text: 'WR'}, { payload: 'TE', text: 'TE'}, { payload: 'D', text: 'D'}]},
 };
